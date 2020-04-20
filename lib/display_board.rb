fa1a85d071d869e5407ed04bcd71081e6a7c6fa3
Allan Acosta <acosta.gov@gmail.com>
2020-04-20 04:45:16 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,14 @@
-# Define display_board that accepts a board and prints
-# out the current state.
+
+def display_board(board)
+rows1 = "   " + "|" + "   " + "|" + "   "
+rows2 = "   " + "|" + "   " + "|" + "   "
+rows3 = "   " + "|" + "   " + "|" + "   "
+separator = "-----------"
+x = " X "
+o = " O "
+ puts rows1
+ puts separator
+ puts rows2
+ puts separator
+ puts rows3
+end
