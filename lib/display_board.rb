61b2f5a3331d3edfa9658da72a456e43e2874ace
Allan Acosta <acosta.gov@gmail.com>
2020-04-20 05:00:14 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -13,12 +13,13 @@ o = " O "
  puts rows3
 end
 def display_board(board)
+  x = " X "
+  o = " O "
 rows1 = "   " + "|" + "   " + "|" + "   "
-rows2 = "   " + "|" + "#{x}   " + "|" + "   "
+rows2 = "   " + "|" + "   " + "|" + "   "
 rows3 = "   " + "|" + "   " + "|" + "   "
 separator = "-----------"
-x = " X "
-o = " O "
+
  puts rows1
  puts separator
  puts rows2
